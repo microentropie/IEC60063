bce66c85fb2ce1a5b64662d6bd3a1b08132c45ad
microentropie <microentropie@users.noreply.github.com>
2017-12-31 08:44:45 +0100

    Add files via upload

--- examples/EseriesTest/EseriesTest.ino
+++ examples/EseriesTest/EseriesTest.ino
@@ -1,12 +1,22 @@
+/*
+Author:  Stefano Di Paolo
+License: MIT, https://en.wikipedia.org/wiki/MIT_License
+Date:  2017-12-31
+
+Library: IEC60063 series resistors.
+
+Sources repository: https://github.com/microentropie/
+*/
+
 #include <IEC60063_E3.h>
 #include <IEC60063_E6.h>
 #include <IEC60063_E12.h>
 #include <IEC60063_E24.h>
+#include <IEC60063_E48.h>
 
 /*
 Required Libraries:
 * https://github.com/microentropie/IEC60063
-* http://arduiniana.org/libraries/pstring/ (http://arduiniana.org/PString/PString3.zip)
 */
 
 /*
@@ -24,19 +34,46 @@ void setup()
   Serial.println("https://github.com/microentropie/IEC60063");
 
 
-  char buf[E24BufSize];
+  char buf[EseriesBufSize];
+
+  // please note Arduino does not directly support printing of floating point numbers
+  // with mantissa and exponent. Thus here the solution has been to increase the
+  // number of decimals.
+  // https://playground.arduino.cc/Main/Printf
   
-  Serial.print(123);
+  float r1 = 123; // 123 ohm
+  Serial.print(r1);
   Serial.print(" => E12: ");
-  Serial.print(E12FormattedValue(buf, sizeof(buf), 123, 'R'));
+  Serial.print(E12Value(r1));
+  Serial.print(" = \"");
+  Serial.print(E12FormattedValue(buf, sizeof(buf), r1, 'R'));
+  Serial.println("\"");
+
+  float c = 24.2E-9; // 24.2nF
+  Serial.print(c, 10);
+  Serial.print(" => E12: ");
+  Serial.print(E12Value(c));
+  Serial.print(" = \"");
+  Serial.print(E12FormattedValue(buf, sizeof(buf), c, 'F'));
+  Serial.println("\"");
+
+  c = 1E-12; // 1pF
+  Serial.print(c, 12);
+  Serial.print(" => E12: ");
+  Serial.print(E12Value(c));
+  Serial.print(" = \"");
+  Serial.print(E12FormattedValue(buf, sizeof(buf), c, 'F'));
+  Serial.print("\", E48: ");
+  Serial.print(E48FormattedValue(buf, sizeof(buf), c, 'F'));
+  Serial.println("\"");
   Serial.println();
 
-  float r[] = {123, 1.4E3, 1050, 1049, 1098 };
+  float r[] = { 0, 1e-12, .31, 74, 123, 1.4E3, 1050, 1049, 1098, 172E3, 100E6 };
   int qt = sizeof(r) / sizeof(float);
   for(int i = 0; i < qt; ++i)
   {
     Serial.print("R=");
-    Serial.print(r[i]);
+    Serial.print(r[i], 12);
     Serial.print(" => E3: ");
     Serial.print(E3FormattedValue(buf, sizeof(buf), r[i], 'R'));
     Serial.print(", => E6: ");
@@ -45,6 +82,8 @@ void setup()
     Serial.print(E12FormattedValue(buf, sizeof(buf), r[i], 'R'));
     Serial.print(", E24: ");
     Serial.print(E24FormattedValue(buf, sizeof(buf), r[i], 'R'));
+    Serial.print(", E48: ");
+    Serial.print(E48FormattedValue(buf, sizeof(buf), r[i], 'R'));
     Serial.println();
   }
 }
@@ -53,3 +92,26 @@ void loop()
 {
   delay(0);
 }
+
+/*
+ * here is the output from an Arduino Uno
+ * 
+EseriesTest.ino
+https://github.com/microentropie/IEC60063
+123.00 => E12: 120.00 = "120R"
+0.0000000242 => E12: 0.00 = "22n"
+0.000000000001 => E12: 0.00 = "1p0", E48: 1p00"
+
+R=0.000000000000 => E3: 0R, => E6: 0R, E12: 0R, E24: 0R, E48: 0R
+R=0.000000000001 => E3: 1p0, => E6: 1p0, E12: 1p0, E24: 1p0, E48: 1p00
+R=0.309999990463 => E3: R22, => E6: R33, E12: R33, E24: R30, E48: R316
+R=74.000000000000 => E3: 100R, => E6: 68R, E12: 68R, E24: 75R, E48: 75R0
+R=123.000000000000 => E3: 100R, => E6: 100R, E12: 120R, E24: 120R, E48: 121R
+R=1400.000000000000 => E3: 1K0, => E6: 1K5, E12: 1K5, E24: 1K5, E48: 1K40
+R=1050.000000000000 => E3: 1K0, => E6: 1K0, E12: 1K0, E24: 1K1, E48: 1K05
+R=1049.000000000000 => E3: 1K0, => E6: 1K0, E12: 1K0, E24: 1K1, E48: 1K05
+R=1098.000000000000 => E3: 1K0, => E6: 1K0, E12: 1K2, E24: 1K1, E48: 1K10
+R=172000.000000000000 => E3: 220K, => E6: 150K, E12: 180K, E24: 180K, E48: 169K
+R=100000000.000000000000 => E3: 100M, => E6: 100M, E12: 100M, E24: 100M, E48: 100M
+*/
+
