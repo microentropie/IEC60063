eb6760adee3e56a2f4ed9e0bee75a29d4805a648
microentropie <microentropie@users.noreply.github.com>
2017-12-31 08:43:06 +0100

    Delete pow10.cpp
    renamed to intPowLog10.cpp

--- src/pow10.cpp
+++
@@ -1,9 +0,0 @@
-static const float _pow10[] = { 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6 };
-
-float pow10(int y)
-{
-  // must be: y = -6 .. +6
-  if (y < -6 || y > 6)
-    return 0;
-  return _pow10[y + 6];
-}
